8b1f4deb4acaefeeddd0d78cfe4d25ce82acd8e5
Pintuprusty16@gmail.com <pintuprusty16@gmail.com>
2023-09-21 01:30:25 +0530

    data type rule

--- day1/1.cpp
+++ day1/1.cpp
@@ -6,3 +6,14 @@ int main(){
     cout<<22;
     return 0;
 }
+/*
+8 bit =  1 byte
+ 
+ char = 1 byte (8bit)
+ int  =  4 byte  (32bit)
+ longint
+ float (3.4) = 4 byte (32bit)
+ double (3.4 (big point number)) = 8 byte (64bit)
+  boolean = 1byte (8bit);
+*/
+
